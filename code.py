da2652b3a30bdc725e80083251bcf3f97392664f
Sajjad Ranjbar <83742735+phantomf4321@users.noreply.github.com>
2022-08-08 15:57:08 +0430

    Update code.py

--- code.py
+++ code.py
@@ -2,7 +2,6 @@ import cv2 #for image processing
 import easygui #to open the filebox
 import numpy as np #to store image
 import imageio #to read image stored at particular path
-
 import sys
 import matplotlib.pyplot as plt
 import os
